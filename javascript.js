a8cb83fa110744a8f561f577a67fa12311688ab0
anthonyk191 <67760299+anthonyk191@users.noreply.github.com>
2025-07-06 13:16:35 -0700

    Created interractable buttons to display on calculator screen

--- javascript.js
+++ javascript.js
@@ -7,12 +7,16 @@ calculator_screen_font_bot.textContent = ""
 
 let number_buttons = document.querySelectorAll(".number_button")
 let number_buttons_array = []
-// let number_button = document.querySelector(".number_button")
 
+//Helper function to update text on calculator_screen_font_top
+function update_font_top(value) {
+    calculator_screen_font_top.textContent += value
+}
 //Function to create a button for the numbers
 function create_button(button) {
     button.addEventListener('click', () => {
-        console.log(button.textContent)
+        // console.log(button.textContent) //This is not necessary delete later
+        update_font_top(button.textContent)
     })
     return(button)
 }
@@ -22,15 +26,74 @@ for (let i = 0; i < number_buttons.length; i++) {
     number_buttons_array.push(create_button(number_buttons[i]))
 }
 
-//All Alternate Buttons
+
+//All Alternate Buttons----------------------------------------------------------------------------
 let alternate_buttons = document.querySelectorAll(".alternate_button")
+let alternate_buttons_array = []
+
+for (let i = 0; i < alternate_buttons.length; i++) {
+    alternate_buttons_array.push(create_button(alternate_buttons[i])) //There is a lot wrong here, this is temporary. Update this later
+}
+
+console.log(alternate_buttons_array[11])
+
 //Addition Button
 let addition_button = alternate_buttons[12]
 //Equal Button
+//I know there are math libraries out there, but I am building from scratch
+
+function add(input_array){
+    let output = 0
+    for (let i=0; i < input_array.length; i++) {
+        output += input_array[i]
+    }
+    return output
+}
+
+function compressArray(input_array){
+    let output_string = ""
+    for (let i = 0; i < input_array.length; i ++) {
+        output_string += input_array[i].toString()
+    }
+    return(output_string)
+}
+
+function isNumber(input) {
+    compare_array = []
+    for (let i=0; i<= 9; i++) {
+        if (input == i) {
+            return (true)
+        }
+    }
+    return false
+}
+
+//Compute Function
+function compute() {
+    compute_array = []
+    let current_number = 0
+    let current_number_array = []
+    //Goes through calculator screen and creates array separating numbers from alternate numbers
+    for (let i = 0; i < calculator_screen_font_top.textContent.length; i++) {
+        if (isNumber(calculator_screen_font_top.textContent[i])) {
+            current_number_array.push(calculator_screen_font_top.textContent[i])
+        }
+        else {
+            current_number = compressArray(current_number_array)
+            compute_array.push(current_number)
+            compute_array.push(calculator_screen_font_top.textContent[i])
+            current_number_array = []
+        }
+    }
+    console.log(compute_array)
+    console.log("Hello")
+}
 let equal_button = alternate_buttons[11]
+equal_button.addEventListener("click", () => {
+    compute()
+})
 
-// console.log(number_button)
-
+//looking for function
 
 // document.getElementById("number_button").onclick = () => {
 //     console.log("Button pressed")
